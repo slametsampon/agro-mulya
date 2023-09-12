c000177f124d3a1be1b6bc75b42f906f40136867
Slamet <slamet@pon.co.id>
2023-09-12 07:44:01 +0700

    start blog

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,30 @@
 const projectsData = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-]
+    {
+      title: 'WIS - Wireless Irrigation System',
+      description: `WIS akan mengontrol kebutuhan semua air yang dibutuhkan tanaman.
+      WIS dapat mengatur periode/jangka waktu, durasi (volume air) serta mode otomatis dari perangkat apa pun yang terhubung ke WIFI
+      Fitur tersebut antara lain pengontrol penyimpanan air untuk melayani ketersediaan air bagi seluruh tanaman`,
+      imgSrc: '/static/images/wis200.png',
+      href: 'https://github.com/slametsampon/WIS_SAM',
+    },
+    {
+      title: 'IoT - Data Logger',
+      description: `Data logger adalah pencatatan data yang beroperasi mandiri dan dilaporkan dalam format pdf setiap hari`,
+      imgSrc: '/static/images/datalogger200.png',
+      href: 'https://github.com/slametsampon/dataLogger',
+    },
+    {
+      title: 'CMMS - Computerized Maintenance Management System',
+      description: `Sistem Manajemen Pemeliharaan seluruh aset Industri`,
+      imgSrc: '/static/images/cmmsClassDiagram200.png',
+      href: 'https://cmms-nextjs.vercel.app/',
+    },
+    {
+      title: 'Blog - Private',
+      description: `Blog Website uses Next.js, Tailwindcss, MDX technologies`,
+      imgSrc: '/static/images/privateBlog.jpg',
+      href: 'https://nextjs-sam-blog.vercel.app/',
+    },
+  ]
 
 export default projectsData
