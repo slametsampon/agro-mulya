645e53ddf5f54c74bb49674ccb4caedf90222c60
Slamet <slamet@pon.co.id>
2023-09-12 19:32:40 +0700

    update blog-greenhouse

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'Website Sayuran, Buah-buahan, Perikanan dan Peternakan',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://agro-mulya.vercel.app',
   siteRepo: 'https://github.com/slametsampon/agro-mulya',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
