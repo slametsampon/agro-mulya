cc103f24b52d55bdbf0fd03243f2d4210bf717bb
Slamet <slamet@pon.co.id>
2023-09-14 10:01:10 +0700

    fix build error on vercel

--- app/(production)/kebun/page.tsx
+++ app/(production)/kebun/page.tsx
@@ -3,7 +3,6 @@ import { genPageMetadata } from 'app/seo'
 export const metadata = genPageMetadata({ title: 'Kebun' })
 
 export default function Page() {
-
   return (
     <>
       <h1>Kebun Page</h1>
