75c0ce4e94d07b9348d4a4c6bbdb1580664a4de6
Slamet <slamet@pon.co.id>
2023-09-29 18:13:25 +0700

    fix getGabungHitungBlokSayur

--- utils/kebun/getBlokSayurLengkap.ts
+++ utils/kebun/getBlokSayurLengkap.ts
@@ -1,11 +1,10 @@
 import kebunStatic from '@/data/kebun/kebun'
 import getGabungHitungBlokSayur from './getGabungHitungBlokSayur'
-import { BlokSingkat, Sayuran } from 'utils/definition'
 
 export default async function getBlokSayurLengkap(id: number) {
   try {
-    const resultBlok: BlokSingkat = kebunStatic.blokSayuran.find((blk) => blk.id === id)
-    const resultSayur: Sayuran = kebunStatic.sayuran.find((sayur) => sayur.id === resultBlok?.id)
+    const resultBlok = kebunStatic.blokSayuran.find((blk) => blk.id === id)
+    const resultSayur = kebunStatic.sayuran.find((sayur) => sayur.id === resultBlok?.id)
     const blokLengkap = getGabungHitungBlokSayur(resultBlok, resultSayur)
     if (!resultBlok) {
       return { name: 'no name', description: 'no name' }
